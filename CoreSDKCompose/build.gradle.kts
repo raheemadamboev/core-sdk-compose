b31e5361d457fb2210983c1985c530f63c9d6c9c
raheem <raheemideadev@gmail.com>
2025-06-05 23:58:45 +0500

    [1.0.22] Upgraded Gradle, dependencies to the latest version;

--- CoreSDKCompose/build.gradle.kts
+++ CoreSDKCompose/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.raheemadamboev"
             artifactId = "core-sdk-compose"
-            version = "1.0.21"
+            version = "1.0.22"
 
             afterEvaluate {
                 from(components["release"])
